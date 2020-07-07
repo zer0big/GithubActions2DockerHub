1b2e5e2d0976c9468725a1ab4d1a8529ee194998
zerobig <zerobig.kim@gmail.com>
2020-07-07 16:44:23 +0900

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GithubActiion2DockerHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
